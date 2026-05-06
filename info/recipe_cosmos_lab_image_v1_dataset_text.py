aba5282aebc24650bfe0d1d2c98fb24c82aebc21
Xingqian Xu <xingqianx@aws-iad-cs-002-vscode-01.cm.cluster>
2026-05-06 09:18:26 -0700

    adding some info

--- info/recipe_cosmos_lab_image_v1_dataset_text.py
+++ info/recipe_cosmos_lab_image_v1_dataset_text.py
@@ -9,13 +9,13 @@
 sources = {
     "image": {
         "nonsensitive": {
-            "cosmos_lab_image_v1_screen2words_rico" : "webdataset_image_regular_text/screen2words_rico/wdinfo/",
-            "cosmos_lab_image_v1_slide_audit" : "webdataset_image_regular_text/slide_audit/wdinfo/",
-            "cosmos_lab_image_v1_voxel51_rico" : "webdataset_image_regular_text/voxel51_rico/wdinfo/",
-            "cosmos_lab_image_v1_zennodo10k" : "webdataset_image_regular_text/zennodo10k/wdinfo/",
-            "cosmos_lab_image_v1_synthetic_scene_text_v0" : "webdataset_image_synthetic_text/synthetic_scene_text_v0/wdinfo/",
-            "cosmos_lab_image_v1_synthetic_chinese_scene_text_v0" : "webdataset_image_synthetic_text/synthetic_chinese_scene_text_v0/wdinfo/",
-            "cosmos_lab_image_v1_synthetic_traditional_chinese_scene_text_v0" : "webdataset_image_synthetic_text/synthetic_traditional_chinese_scene_text_v0/wdinfo/",
+            "cosmos_lab_image_v1_screen2words_rico" : "webdataset_image_regular_text/screen2words_rico/wdinfo",
+            "cosmos_lab_image_v1_slide_audit" : "webdataset_image_regular_text/slide_audit/wdinfo",
+            "cosmos_lab_image_v1_voxel51_rico" : "webdataset_image_regular_text/voxel51_rico/wdinfo",
+            "cosmos_lab_image_v1_zennodo10k" : "webdataset_image_regular_text/zennodo10k/wdinfo",
+            "cosmos_lab_image_v1_synthetic_scene_text_v0" : "webdataset_image_synthetic_text/synthetic_scene_text_v0/wdinfo",
+            "cosmos_lab_image_v1_synthetic_chinese_scene_text_v0" : "webdataset_image_synthetic_text/synthetic_chinese_scene_text_v0/wdinfo",
+            "cosmos_lab_image_v1_synthetic_traditional_chinese_scene_text_v0" : "webdataset_image_synthetic_text/synthetic_traditional_chinese_scene_text_v0/wdinfo",
         },
     }
 }
@@ -23,13 +23,13 @@ sources = {
 
 collections = {
     "image": {
-        "cosmos_lab_image_v1_reg": {
+        "cosmos_lab_image_v1_reg_text": {
             "cosmos_lab_image_v1_screen2words_rico": 1.0,
             "cosmos_lab_image_v1_slide_audit": 1.0,
             "cosmos_lab_image_v1_voxel51_rico": 1.0,
             "cosmos_lab_image_v1_zennodo10k": 1.0,
         },
-        "cosmos_lab_image_v1_sgd": {
+        "cosmos_lab_image_v1_sgd_text": {
             "cosmos_lab_image_v1_synthetic_scene_text_v0": 1.0,
             "cosmos_lab_image_v1_synthetic_chinese_scene_text_v0": 1.0,
             "cosmos_lab_image_v1_synthetic_traditional_chinese_scene_text_v0": 1.0,
@@ -41,7 +41,7 @@ collections = {
 recipes = {
     "XX_COSMOS_LAB_IMAGE_V1_TEXT": {
         "cosmos_lab_image_v1_reg_text": {"type": "image", "ratio": 1.0},
-        "cosmos_lab_image_v1_sdg_text": {"type": "image", "ratio": 0.05},
+        "cosmos_lab_image_v1_sgd_text": {"type": "image", "ratio": 0.05},
     },
 }
 
